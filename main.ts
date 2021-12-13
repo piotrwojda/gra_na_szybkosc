7d38253ae58369cabad015878909ccd4fb982ecc
piotrwojda <piotr.wojda@gmail.com>
2021-12-13 19:27:01 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -10,10 +10,10 @@ let pressed_button = 0
 let _4digit = grove.createDisplay(DigitalPin.P1, DigitalPin.P15)
 basic.showIcon(IconNames.Happy)
 basic.pause(2000)
-pressed_button = 0
 basic.forever(function () {
     for (let index = 0; index < 3; index++) {
         reaction_time = 0
+        pressed_button = 0
         basic.clearScreen()
         basic.pause(randint(200, 1000))
         button = randint(1, 2)
